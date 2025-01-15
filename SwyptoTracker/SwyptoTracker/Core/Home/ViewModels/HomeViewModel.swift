43851b442d11c4f4c7479f7fbe0ea14a1105350f
Sabbir <62333547+sabbirn26@users.noreply.github.com>
2025-01-15 15:06:48 +0600

    - created CoinDataService and API call functionalities, showing real API data in Home view

--- SwyptoTracker/SwyptoTracker/Core/Home/ViewModels/HomeViewModel.swift
+++ SwyptoTracker/SwyptoTracker/Core/Home/ViewModels/HomeViewModel.swift
@@ -6,15 +6,29 @@
 //
 
 import Foundation
+import Combine
 
 class HomeViewModel : ObservableObject {
     @Published var allCoins : [CoinModel] = []
     @Published var portfoliCoins: [CoinModel] = []
     
+    private var dataService = CoinDataService()
+    
+    private var cancellable = Set<AnyCancellable>()
+    
     init() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            self.allCoins.append(DeveloperPreview.instance.coin)
-            self.portfoliCoins.append(DeveloperPreview.instance.coin)
-        }
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+//            self.allCoins.append(DeveloperPreview.instance.coin)
+//            self.portfoliCoins.append(DeveloperPreview.instance.coin)
+//        }
+        addSubcribers()
+    }
+    
+    func addSubcribers(){
+        dataService.$allCoins
+            .sink { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellable)
     }
 }
