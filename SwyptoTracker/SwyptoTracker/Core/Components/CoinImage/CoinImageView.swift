c42a4ade17735c05b8df57e27c5e15f9aaac1c20
Sabbir <62333547+sabbirn26@users.noreply.github.com>
2025-01-16 12:52:33 +0600

    - created+refactored : CoinImageView, CoinImageViewModel, CoinRowView...
    - CoinImageView: it shows image from the API
    - CoinImageViewModel: it is the view model of the view that handles all the data that comes from the CoinImageService
    - CoinImageService: It downloads image from the coin api image url and so on
    - CoinRowView: Refactored the view replaced the circle() with CoinImgeView and passed the coin to the view to show images on the view that comes from the API

---
+++ SwyptoTracker/SwyptoTracker/Core/Components/CoinImage/CoinImageView.swift
@@ -0,0 +1,41 @@
+//
+//  CoinImageView.swift
+//  SwyptoTracker
+//
+//  Created by Sabbir Nasir on 16/1/25.
+//
+
+import SwiftUI
+
+struct CoinImageView: View {
+    @StateObject var vm: CoinImageViewModel
+    
+    init(coin: CoinModel){
+        _vm = StateObject(wrappedValue: CoinImageViewModel(coin: coin))
+    }
+    var body: some View {
+        ZStack{
+            if let image = vm.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            } else if vm.isLoading {
+                ProgressView()
+            } else {
+                Image(systemName: "questionmark")
+                    .foregroundStyle(Color.theme.scndTextColor)
+            }
+        }
+    }
+}
+
+
+struct CoinImageView_Previews : PreviewProvider {
+    static var previews: some View{
+        NavigationView{
+            CoinImageView(coin: dev.coin)
+                .padding()
+                .previewLayout(.sizeThatFits)
+        }
+    }
+}
