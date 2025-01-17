3527547d998bdde52782f6a7371564464626a010
Sabbir Nasir <nsabbir26@gmail.com>
2025-01-17 20:07:24 +0600

    - func: SearchBar functionality
    - worked on search bar functionality to how filtered Coin data after given input from the search bar

--- SwyptoTracker/SwyptoTracker/Core/Home/ViewModels/HomeViewModel.swift
+++ SwyptoTracker/SwyptoTracker/Core/Home/ViewModels/HomeViewModel.swift
@@ -18,18 +18,43 @@ class HomeViewModel : ObservableObject {
     private var cancellable = Set<AnyCancellable>()
     
     init() {
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-//            self.allCoins.append(DeveloperPreview.instance.coin)
-//            self.portfoliCoins.append(DeveloperPreview.instance.coin)
-//        }
+        //        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+        //            self.allCoins.append(DeveloperPreview.instance.coin)
+        //            self.portfoliCoins.append(DeveloperPreview.instance.coin)
+        //        }
         addSubcribers()
     }
     
+    //updates allCoins
     func addSubcribers(){
-        dataService.$allCoins
+        //        dataService.$allCoins
+        //            .sink { [weak self] (returnedCoins) in
+        //                self?.allCoins = returnedCoins
+        //            }
+        //            .store(in: &cancellable)
+        
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
             .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellable)
+    }
+    
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel]{
+        guard !text.isEmpty else {
+            return coins
+        }
+        
+        let lowercasedText = text.lowercased()
+        
+        return coins.filter { (coin) -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+            coin.symbol.lowercased().contains(lowercasedText) ||
+            coin.id.lowercased().contains(lowercasedText)
+            
+        }
     }
 }
