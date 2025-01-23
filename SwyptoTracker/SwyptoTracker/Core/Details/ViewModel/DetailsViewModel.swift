46cf31ff045082c0d5eb6d82142f92e57f00259e
Sabbir <62333547+sabbirn26@users.noreply.github.com>
2025-01-23 12:49:31 +0600

    - updated: the DetailsViewModel and stored the data over here that comes from the API of coin details

--- SwyptoTracker/SwyptoTracker/Core/Details/ViewModel/DetailsViewModel.swift
+++ SwyptoTracker/SwyptoTracker/Core/Details/ViewModel/DetailsViewModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import Combine
+
+class DetailsViewModel {
+    private let coinDetailsService: CoinDetailsDataService
+    private var cancellables = Set<AnyCancellable>()
+    init(coin: CoinModel) {
+        self.coinDetailsService = CoinDetailsDataService(coin: coin)
+        self.addSubscribers()
+    }
+    
+    private func addSubscribers(){
+        
+        coinDetailsService.$coinDetails
+            .sink { (returnedCoinDetails) in
+                print("<----- Received coin details data. -----> \n\(String(describing: returnedCoinDetails))")
+                
+            }
+            .store(in: &cancellables)
+    }
+}
