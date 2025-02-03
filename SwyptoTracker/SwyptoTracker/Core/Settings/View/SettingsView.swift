0096487400e859e40e8e96b5e2581d09f5845293
Sabbir <62333547+sabbirn26@users.noreply.github.com>
2025-02-03 17:34:45 +0600

    - created: SettingsView and also added image asset
    - Settings view contains image and link about various things that was used in the project also there is developer section.

---
+++ SwyptoTracker/SwyptoTracker/Core/Settings/View/SettingsView.swift
@@ -0,0 +1,116 @@
+//
+//  SettingsView.swift
+//  SwyptoTracker
+//
+//  Created by Sabbir Nasir on 3/2/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    let defaultURL = URL(string: "https://google.com/")!
+    let swiftfulYoutubeURL = URL(string: "https://www.youtube.com/@SwiftfulThinking")!
+    let githubURL = URL(string: "https://youtube.com/")!
+    let coingeckoURL = URL(string: "https://coingecko.com/")!
+    let coffeeURL = URL(string: "https://youtube.com/")!
+    let personalURL = URL(string: "https://github.com/sabbirn26")!
+    var body: some View {
+        NavigationView {
+            List{
+                personalInfoSection
+                coingeckoSection
+                devSection
+                applicationSection
+            }
+            .font(.headline)
+            .accentColor(.blue)
+            .listStyle(GroupedListStyle())
+            .navigationTitle("Settings")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    XmarkButton(dismiss: {})
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SettingsView()
+}
+
+extension SettingsView {
+    //MARK: VIEW PARTS
+    private var personalInfoSection : some View {
+        Section(header: Text("Swypto Tracker")) {
+            VStack(alignment: .leading){
+                Image("logo")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Text("This is a Crypto Tracker app. It uses MVVM Architecture, Combine and CoreData!")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundStyle(Color.theme.accent)
+            }
+            .padding(.vertical)
+            HStack {
+                Text("Huge shout-out to: ")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundStyle(Color.theme.accent)
+                
+                Link("SwiftfulThinking", destination: swiftfulYoutubeURL)
+                    .foregroundStyle(.blue)
+            }
+        }
+    }
+    
+    private var coingeckoSection : some View {
+        Section(header: Text("CoinGecko")) {
+            VStack(alignment: .leading){
+                Image("coingecko")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 100)
+                
+                Text("The crypto currency data that is used in this app comes from a fee API from CoinGecko! Prices has a delay.")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundStyle(Color.theme.accent)
+            }
+            .padding(.vertical)
+            Link("Visit CoinGecko", destination: coingeckoURL)
+        }
+    }
+    
+    private var devSection : some View {
+        Section(header: Text("Developer")) {
+            VStack(alignment: .leading){
+                Image("devPhoto")
+                    .resizable()
+                    .frame(width : 100, height: 100)
+                    .cornerRadius(10)
+                
+                Text("I'm Sabbir, a software engineer and content creator. This project is part of my learning journey.")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundStyle(Color.theme.accent)
+            }
+            .padding(.vertical)
+            Link("GitHub Profile 🧑🏽‍💻", destination: personalURL)
+            Link("Support Sabbir's coffee addiction ☕️", destination: coffeeURL)
+        }
+    }
+    
+    private var applicationSection : some View {
+        Section(header: Text("Application")) {
+            
+            Link("Terms of Service", destination: defaultURL)
+            Link("Privacy Policy", destination: defaultURL)
+            Link("Company Website", destination: defaultURL)
+            Link("Learn More", destination: defaultURL)
+        }
+    }
+}
